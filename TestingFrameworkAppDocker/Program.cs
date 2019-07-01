c7f00c65351ba79dbe88ca6a2fc5846ba5fca655
IKwiotek <iankwiotek@quickenloans.com>
2019-07-01 11:49:26 -0400

    Initial Commit

---
+++ TestingFrameworkAppDocker/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TestingFrameworkAppDocker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Program Starting");
+
+            Thread.Sleep(10000);
+
+            Console.WriteLine("Continuing Program Execution");
+
+            Thread.Sleep(10000);
+
+            Console.WriteLine("Closing Program!");
+        }
+    }
+}
